79a6097c650d881b5c5bd325fda2804fcfada56f
VITOR CAVALLINI DE SOUZA <vitor.csouza14@senacsp.edu.br>
2023-11-06 22:16:34 -0300

    Primeiro commit

---
+++ SistemaVendasLivros/Menu Principal.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVendasLivros
+{
+    class Menu_Principal
+    {
+        model modelo;
+        private int opcao;
+        public control()
+        {
+            this.modelo = new model();//Criando uma chave
+            ConsultarOpcao = -1;
+        }//fim do construtor
+
+        public int ConsultarOpcao
+        {
+            get { return this.opcao; }
+            set { this.opcao = value; }
+        }//fim do método
+        public void Menu()
+        {
+            Console.WriteLine("Escolha uma das opções abaixo: \n\n" +
+                              "0. Sair\n" +
+                              "1. Digite seu login: \n" +
+                              "2. Cadastro\n");
+            ConsultarOpcao = Convert.ToInt32(Console.ReadLine());
+        }//fim do menu
+
+        public void Operacao()
+        {
+            do
+            {
+                Menu();//Mostrar o menu em tela
+                switch (ConsultarOpcao)
+                {
+                    case 0:
+                        Console.WriteLine("Obrigado!");
+                        break;
+                    case 1:
+                        Console.WriteLine(this.modelo.ConsultarLogin());
+                        break;
+                    default:
+                        Console.WriteLine("Erro, escolha uma opção valida!");
+                        break;
+                }//fim do switch
+            } while (ConsultarOpcao != 0);
+        }//fim do método operacao
+
+    }
+}
---
+++ SistemaVendasLivros/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVendasLivros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            control controle = new control();
+            controle.Operacao();
+
+            Console.ReadLine();//Manter o Prompt Aberto
+        }//fim do método main
+    }//fim da classe
+}//fim do projeto
